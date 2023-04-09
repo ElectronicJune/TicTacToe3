a1cfa8f89e7a72aa04601d9b3b1e6a0fd3b4ec3c
EJune <ejunef09@gmail.com>
2023-04-09 20:15:10 +0800

    optimize code, edit documentations

--- main.cpp
+++ main.cpp
@@ -2,29 +2,28 @@
 #include <string>
 #include <vector>
 using namespace std;
+
 //player is O 
 //computer is X
 //code is based on computer
 
-//display the board to the terminal
+//display board
 void display(string board){
   for (int i=0;i<9;i++){
     cout << " " << board[i] << " ";
-    if (i%3 == 0 || i%3 == 1){
-      cout << "|";
-    }else {
+    if (i%3 == 2){
       cout << "\n";
-      if (i != 8){
-        cout << "---+---+---\n";
-      }
+      if (i != 8) cout << "---+---+---\n";
+    }else {
+      cout << "|";
     }
-    
   }
 }
-//return a vector of strings(board) for the next move
+
+//return a vector of boards after the next move
 vector<string> moveCombinations(string board, char player){
   vector<string> combinations;
-  for (int i=0;i<board.length();i++){
+  for (int i=0;i<9;i++){
     if (board[i] == ' '){
       string combination = board;
       combination[i] = player;
@@ -33,26 +32,23 @@ vector<string> moveCombinations(string board, char player){
   }
   return combinations;
 }
-//returns the result of the board if ended, returns 2 if not ended
+
+//returns the board result if ended, else return 2
 int result(string board){
-  //result is based on computer (1 for computer win)
-  //check for horizontal match (in a row)
+  //result is based on computer (1 for win)
+  //check horizontal match
   for (int i=0;i<9;i+=3){
     //check if there is no pieces on the position
-    if (board[i]==' '){
-      continue;
-    }
+    if (board[i]==' ') continue;
     //check if the whole row is the same
     if (board[i]==board[i+1] && board[i]==board[i+2]){
       return (board[i]=='X')? 1 : -1; 
     }
   }
-  //check for vertical match (in a column)
+  //check for vertical match
   for (int i=0;i<9;i++){
     //check if there is no pieces on the position
-    if (board[i]==' '){
-      continue;
-    }
+    if (board[i]==' ') continue;
     //check if the whole column is the same
     if (board[i]==board[i+3] && board[i]==board[i+6]){
       return (board[i]=='X')? 1 : -1; 
@@ -64,54 +60,42 @@ int result(string board){
       return board[4]=='X' ? 1 : -1;
     }
   }
-  //check if not game not end
+  //check if game not end
   for (char i : board){
-    if (i==' '){
-      return 2;
-    }
+    if (i==' ') return 2;
   }
   //draw
   return 0;
 }
+
 //minimax function, used to find the best move
-//if player is X, it is the maximizing player
+//X is the maximizing player
 int minimax(string board, char player){
   //if the board has ended, return the result
-  if (result(board)!=2){
-    return result(board);
-  }
+  if (result(board)!=2) return result(board);
   //if not, find the max/min result of the next move combination
   int result = player =='X' ? -1:1;
   for (string moves : moveCombinations(board,player)){
     // if maximizing player
     if (player == 'X'){
       int next_result =  minimax(moves,'O');
-      if (next_result>result){
-        result = next_result;
-      }
-      if (next_result==1){
-        return result;
-      }
+      if (next_result>result)result = next_result;
+      if (next_result==1) return result;
     }else{
       int next_result =  minimax(moves,'X');
-      if (next_result<result){
-        result = next_result;
-      }
-      if (next_result==-1){
-        return result;
-      }
+      if (next_result<result) result = next_result;
+      if (next_result==-1) return result;
     }
   }
   return result;
 }
-//returning the next best move, using the minimax funciton
+
+//return next best move with minimax
 string nextBestMove(string board){
   int board_result = minimax(board,'X');
   //expected is X
   for (string move : moveCombinations(board,'X')){
-    if (minimax(move,'O')==board_result){
-      return move;
-    }
+    if (minimax(move,'O')==board_result) return move;
   }
   return "";
 }
@@ -132,7 +116,6 @@ int main(){
       do{
         cin >> player_move;
       }while (board[player_move-1]!=' ' || player_move<1 || player_move>10);
-      
       board[player_move-1] = 'O';
       player = 'X';
     }
@@ -149,6 +132,3 @@ int main(){
   }
   return 0;
 }
-
-
-  
